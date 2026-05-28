dd3de8ceae0143ce52e1b5b57e09e59455cc95af
Jess Chadwick <jchadwick@gmail.com>
2026-05-27 23:40:59 -0400

    fix(build): fail fast on typecheck and declare arctic types

---
+++ packages/server/src/types/arctic.d.ts
@@ -0,0 +1,23 @@
+declare module 'arctic' {
+  export type IdTokenClaims = {
+    sub: string;
+  } & Record<string, unknown>;
+
+  export interface OAuth2Tokens {
+    idToken(): string | null;
+  }
+
+  export class OAuth2RequestError extends Error {}
+
+  export class ArcticFetchError extends Error {}
+
+  export class Google {
+    constructor(clientId: string, clientSecret: string, redirectUri: string);
+    createAuthorizationURL(state: string, codeVerifier: string, scopes: string[]): URL;
+    validateAuthorizationCode(code: string, codeVerifier: string): Promise<OAuth2Tokens>;
+  }
+
+  export function generateState(): string;
+  export function generateCodeVerifier(): string;
+  export function decodeIdToken(token: string): IdTokenClaims;
+}

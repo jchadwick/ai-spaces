6f2e1bda1a0f57df753378caaeda9af0a4c73080
Jess Chadwick <jchadwick@gmail.com>
2026-06-06 10:33:05 -0400

    fix: change image padding

--- packages/web/src/components/editors/ImageViewer.tsx
+++ packages/web/src/components/editors/ImageViewer.tsx
@@ -4,7 +4,7 @@ export default function ImageViewer({ content, fileInfo }: ViewerProps) {
   if (!content) return null;
 
   return (
-    <div className="flex-1 overflow-y-auto custom-scrollbar p-8 flex items-center justify-center">
+    <div className="flex-1 overflow-y-auto custom-scrollbar px-5 flex items-center justify-center">
       <img
         src={content}
         alt={fileInfo.name}
